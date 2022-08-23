c6eaef16927a77ca6d3052162f1ec9c79a838f72
Kirill Dolbilov <dolbilov.kirill@gmail.com>
2022-08-23 13:50:28 +0300

    feat: add JS functionality for amount input
    - add blocks for smooth animation

--- pages/index.js
+++ pages/index.js
@@ -5,10 +5,44 @@ const submitButton = donatForm.querySelector('.donation-form__submit-button');
 
 const supportSection = document.querySelector('.support');
 const supportForm = supportSection.querySelector('.donation-form');
+const amountInput = supportForm.querySelector('#amount');
+const amountRadios = supportForm
+  .querySelector('.donation-form__fieldset_for_amount')
+  .querySelectorAll("input[name='donation-amount']");
+const SHOW_ANIMATION_DELAY = 20;
 
 let btnSliderFwd = document.querySelector('#btn-slider-fwd');
 let btnSliderBack = document.querySelector('#btn-slider-back');
 let slideIndex = 1;
+
+function showElement(elem) {
+  if (elem.classList.contains('hidden')) {
+    elem.classList.remove('hidden');
+    setTimeout(function () {
+      elem.classList.remove('visuallyhidden');
+    }, SHOW_ANIMATION_DELAY);
+  }
+}
+
+function hideElement(elem) {
+  if (!elem.classList.contains('hidden')) {
+    elem.classList.add('visuallyhidden');
+    elem.addEventListener('transitionend', () => elem.classList.add('hidden'), {
+      capture: false,
+      once: true,
+      passive: false,
+    });
+  }
+}
+
+function handleAmountRadioChange(evt) {
+  const id = evt.target.id;
+  if (id === 'donation-amount-other') {
+    showElement(amountInput);
+  } else {
+    hideElement(amountInput);
+  }
+}
 
 function changeNameButtonNews() {
   // функция меняет текст кнопки "Все новости" на "Посмотреть все"
@@ -18,12 +52,11 @@ function changeNameButtonNews() {
 }
 
 function closeDescription(evt) {
-  const currentAccordion = evt.target
-    .closest('.accordion');
-  const clickedButton = currentAccordion
-    .querySelector('.accordion__button');
-  const descriptionElement = currentAccordion
-    .querySelector('.accordion__description');
+  const currentAccordion = evt.target.closest('.accordion');
+  const clickedButton = currentAccordion.querySelector('.accordion__button');
+  const descriptionElement = currentAccordion.querySelector(
+    '.accordion__description'
+  );
 
   if (
     descriptionElement &&
@@ -31,14 +64,14 @@ function closeDescription(evt) {
   ) {
     descriptionElement.classList.add('accordion__description_close');
     clickedButton.classList.remove('accordion__button_close');
-    currentAccordion.classList.remove('accordion_close')
+    currentAccordion.classList.remove('accordion_close');
   } else if (
     descriptionElement &&
     descriptionElement.classList.contains('accordion__description_close')
   ) {
     descriptionElement.classList.remove('accordion__description_close');
     clickedButton.classList.add('accordion__button_close');
-    currentAccordion.classList.add('accordion_close')
+    currentAccordion.classList.add('accordion_close');
   }
 }
 
@@ -52,6 +85,12 @@ function handleDonatFormSubmit(evt) {
     const id = checkedCheckbox.id.replace('donat', 'donation');
     const checkbox = supportForm.querySelector(`#${id}`);
     checkbox.checked = true;
+
+    if (id === 'donation-amount-other') {
+      showElement(amountInput);
+    } else {
+      hideElement(amountInput);
+    }
   }
 
   supportForm.scrollIntoView();
@@ -90,6 +129,10 @@ document.querySelectorAll('.accordion__button').forEach((button) => {
 });
 
 donatForm.addEventListener('submit', handleDonatFormSubmit);
+
+amountRadios.forEach((radio) =>
+  radio.addEventListener('change', handleAmountRadioChange)
+);
 
 if (
   document.documentElement.clientWidth >= 768 &&
