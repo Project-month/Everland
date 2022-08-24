f7130a0600651f15ea1351b3f7bbaa95903a2b75
Darker <darkerros@yandex.ru>
2022-08-24 22:34:10 +0300

    feat: add main slider

--- pages/slider.js
+++ pages/slider.js
@@ -42,3 +42,83 @@ document.querySelectorAll(".button_theme_black-click-back").forEach((btn) =>
   })
 );
 /* конец */
+
+
+const capitalContentSection = document.querySelector('.capital__content');
+const capitalSubtitle = capitalContentSection.querySelector('.capital__subtitle');
+const capitalPreTitle = capitalContentSection.querySelector('.capital__everland');
+const capitalTitle = capitalContentSection.querySelector('.capital__title');
+const capitalSliderCount = capitalContentSection.querySelector('.big-text');
+const capitalPreviousSlideButton = capitalContentSection.querySelector('.button__click-back');
+const capitalNextSlideButton = capitalContentSection.querySelector('.button__click-forward');
+
+const capitalFirstImage = capitalContentSection.querySelector('.capital__image-one');
+const capitalSecondImage = capitalContentSection.querySelector('.capital__image-two');
+
+
+
+
+const sliderInfoList = [{preTitleColored : "Мы работаем с",title : " 2016 года по России и СНГ и помогаем людям с инвалидностью. ",subtitle : "Everland — социальный предпринимательский проект",imageFirst: {alt: "Семинары", src : "./images/Image_2.jpg"},imageSecond: {alt: "Беседы", src : "./images/Image_1.jpg"}},{preTitleColored : "Everland",title : " — социальный предпринимательский проект",subtitle : "Мы работаем с 2016 года по России и СНГ и помогаем людям с инвалидностью устойчиво интегрироваться в открытый рынок труда",imageFirst: {alt: "Беседы", src : "./images/Image_1.jpg"},imageSecond: {alt: "Семинары", src : "./images/Image_2.jpg"}}]
+
+
+
+function getSlideInfo(){
+  const sliderCountList =  capitalSliderCount.textContent.split("/")
+  let currentCount = Number(sliderCountList[0])
+  if (currentCount % 1 === 0 && currentCount % 2 !== 0){
+    return sliderInfoList[1]
+  }
+  else {
+    return sliderInfoList[0]
+  }
+}
+
+function nextSlide(){
+  const sliderCountList =  capitalSliderCount.textContent.split("/")
+  let currentCount = Number(sliderCountList[0])
+  let maxCount = sliderCountList[1]
+
+  if (currentCount < maxCount){
+    currentCount += 1
+    capitalSliderCount.textContent = `${currentCount}/${maxCount}`
+    const slideInfo = getSlideInfo()
+    const preTitleNode = capitalPreTitle.cloneNode(true)
+    preTitleNode.textContent = slideInfo.preTitleColored
+
+    capitalTitle.textContent = ""
+    capitalTitle.append(preTitleNode,slideInfo.title)
+
+    capitalSubtitle.textContent = slideInfo.subtitle
+    capitalFirstImage.src = slideInfo.imageFirst.src
+    capitalFirstImage.alt = slideInfo.imageFirst.alt
+    capitalSecondImage.src = slideInfo.imageSecond.src
+    capitalSecondImage.alt = slideInfo.imageSecond.alt
+  }
+
+}
+function forwardSlide(){
+  const sliderCountList =  capitalSliderCount.textContent.split("/")
+  let currentCount = Number(sliderCountList[0])
+  let maxCount = sliderCountList[1]
+
+  if (currentCount > 1){
+    currentCount -= 1
+    capitalSliderCount.textContent = `${currentCount}/${maxCount}`
+    const slideInfo = getSlideInfo()
+    const preTitleNode = capitalPreTitle.cloneNode(true)
+    preTitleNode.textContent = slideInfo.preTitleColored
+
+    capitalTitle.textContent = ""
+    capitalTitle.append(preTitleNode,slideInfo.title)
+
+    capitalSubtitle.textContent = slideInfo.subtitle
+    capitalFirstImage.src = slideInfo.imageFirst.src
+    capitalFirstImage.alt = slideInfo.imageFirst.alt
+    capitalSecondImage.src = slideInfo.imageSecond.src
+    capitalSecondImage.alt = slideInfo.imageSecond.alt
+  }
+
+}
+
+capitalNextSlideButton.addEventListener("click",() => nextSlide())
+capitalPreviousSlideButton.addEventListener("click",() => forwardSlide())
