e32a2eee7a1a084b901e38d31e088128e001836a
Andrey Fadeyev <fedbka@live.ru>
2022-08-25 10:32:16 +0400

    refactor: slider.js has been optimized

--- blocks/slider/slider.js
+++ blocks/slider/slider.js
@@ -1,37 +1,28 @@
-function addSlideActionsToButtons(buttons, slideToHide, slideToShow) {
+function addSlideActionsToButtons(buttons, slidesToToggle) {
   buttons.map(button => {
-    button.addEventListener('click', () => {
-      slideToHide.classList.toggle('slider__item_hidden');
-      slideToShow.classList.toggle('slider__item_hidden');
-    });
+    button.addEventListener('click', () => slidesToToggle.forEach(slide => slide.classList.toggle('slider__item_hidden')));
   });
 }
 
 function addActionsToSlides(slides) {
 
-  for (let currentIndex = 0; currentIndex < slides.length; currentIndex++) {
-    let nextIndex = currentIndex == slides.length - 1 ? 0 : currentIndex + 1;
-    let prevIndex = currentIndex == 0 ? slides.length - 1 : currentIndex - 1;
+  slides.forEach((currentSlide, index, allSlides) => {
+    previousSlide = allSlides[index ==  0 ? allSlides.length - 1 : index + 1];
+    nextSlide = allSlides[index == allSlides.length - 1 ? 0 : index + 1];
 
-    let currentSlide = slides[currentIndex];
-    let previousSlide = slides[prevIndex];
-    let nextSlide = slides[nextIndex];
-
-    const buttonsForward = [...currentSlide.querySelectorAll('.slider__action_type_forward')];
-    addSlideActionsToButtons(buttonsForward, currentSlide, nextSlide);
-
-    const buttonsBack = [...currentSlide.querySelectorAll('.slider__action_type_back')];
-    addSlideActionsToButtons(buttonsBack, currentSlide, previousSlide);
-
-  }
+    addSlideActionsToButtons([...currentSlide.querySelectorAll('.slider__action_type_forward')], [currentSlide, nextSlide]);
+    addSlideActionsToButtons([...currentSlide.querySelectorAll('.slider__action_type_back')], [currentSlide, previousSlide]);
+    
+  });
 }
 
 function addSliderFunctionalityToSection(section) {
 
   slider = section.querySelectorAll('.slider__items');
-  if (!slider) return;
   slides = section.querySelectorAll('.slider__item');
-  if (!slides) return;
+  
+  if (!slider || !slides) return;
+  
   addActionsToSlides(slides);
 
 }
